d26fbc5055ab569bd6b73c12d4a41e7de0c18602
Salum Muhammed <salumalimuhammed@yahoo.com>
2018-11-28 14:33:27 +0000

    TWEAK: Streamlining the Referencing of SASS Particles in file Structure

--- src/app/section/button/bckpg/bckpg.component.ts
+++ src/app/section/button/bckpg/bckpg.component.ts
@@ -5,6 +5,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-back-button',
   templateUrl: './bckpg.component.html',
+  styleUrls: ['../_Button.scss']
 
 })
 export class BckpgComponent {
--- src/app/section/footbar/footbar.component.ts
+++ src/app/section/footbar/footbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footbar',
   templateUrl: './footbar.component.html',
-  styleUrls: ['./footbar.component.scss']
+  styleUrls: ['./footbar.component.scss',]
 })
 export class FootbarComponent implements OnInit {
 
